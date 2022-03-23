b599b97d8d554442ce02535a05db217550651715
Moss Prescott <moss@theprescotts.com>
2022-03-22 21:54:42 -0400

    Bounce

--- Source/main.swift
+++ Source/main.swift
@@ -1,40 +1,34 @@
 let TEXT_WIDTH = 86
 let TEXT_HEIGHT = 16
 
-// var x: Int
-// var y: Int
-// var dx: Int
-// var dy: Int
+var x = (400-TEXT_WIDTH)/2
+var y = (240-TEXT_HEIGHT)/2
+var dx = 1
+var dy = 2
 
 /// Called once when the app is loaded, before the first call to update().
 /// You can put code to do any one-time setup here.
 func setup() {
     // font = Playdate.Graphics.loadFont(fontpath, &err);
-
 }
 
 /// Called once per frame to consume input and draw to the screen. If anything was drawn and the
 /// screen needs to be updated by the sytem, return true.
 func update() -> Bool {
-    // TODO: statics
-    let x = (400-TEXT_WIDTH)/2
-    let y = (240-TEXT_HEIGHT)/2
-    // let dx = 1
-    // let dy = 2
-
-
     Playdate.Graphics.clear(.white)
-    // // Playdate.Graphics.setFont(font)
+    // Playdate.Graphics.setFont(font)
     Playdate.Graphics.drawText("Hello World!", x: x, y: y)
 
-    // // x += dx;
-	// // y += dy;
+    x += dx
+	y += dy
 
-	// // if ( x < 0 || x > LCD_COLUMNS - TEXT_WIDTH )
-	// // 	dx = -dx;
+	if x < 0 || x > Int(LCD_COLUMNS) - TEXT_WIDTH {
+        dx = -dx
+    }
 
-	// // if ( y < 0 || y > LCD_ROWS - TEXT_HEIGHT )
-	// // 	dy = -dy;
+	if y < 0 || y > Int(LCD_ROWS) - TEXT_HEIGHT {
+		dy = -dy
+    }
 
 	Playdate.System.drawFPS(x: 0, y: 0)
 
