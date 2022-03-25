de5b1b69948fca6c22825fbe6bfe548b13d9b09d
Moss Prescott <moss@theprescotts.com>
2022-03-25 16:56:02 -0400

    Remove dead code

--- swift-pd/Sources/CPlaydate/Playdate-Bridging-Header.h
+++ swift-pd/Sources/CPlaydate/Playdate-Bridging-Header.h
@@ -1,11 +1,6 @@
 #define TARGET_EXTENSION 1
 #include "pd_api.h"
 
-// // Call a C vararg function via a pointer, passing no additional arguments. Easier to do it here
-// // than figure out how to explain it to Swift.
-// void invokePrintf(void *f(const char*, ...), const char *str) {
-//     f(str);
-// }
 
 // Wrap calls to C vararg functions passing no additional arguments. Easier to do it here
 // than figure out how to explain it to Swift.
